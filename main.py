aea2aecaf256ee1a1852398cb7fa7ebc628e342f
stanislavets <stanislavets2009@mail.ru>
2024-06-25 11:03:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
-number_tasks = 12
-number_hours = 1.5
-course_name = "Python"
-time_one_task = (number_hours/number_tasks)
-print( "Курс:",course_name,",","всего задач:",number_tasks,"затрачено часов:",number_hours,"среднее время выполнения:", time_one_task, "часа" )
+example= "топинамбур"
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1:10:2])
